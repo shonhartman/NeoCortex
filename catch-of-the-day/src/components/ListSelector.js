b19e4b92e486cddde8f091f4dd24a2cc5dab7e20
Shaun Hartman <shartman@teamsi.com>
2018-07-13 10:48:08 -0500

    order & inventory

--- catch-of-the-day/src/components/ListSelector.js
+++ catch-of-the-day/src/components/ListSelector.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-class ListPicker extends React.Component {
+class ListSelector extends React.Component {
     render() {
         return (
             <form action="" className="store-selector">
                 <h2>Enter Habit List</h2>
-                <input type="text" required placeholder="Habit List"/>
+                <input type="text" required placeholder="Select a List"/>
                 <button type="submit">Go to List -></button>
             </form>
         )
     }
 }
 
-export default ListPicker;
+export default ListSelector;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-import ListPicker from "./components/ListPicker";
+import ListSelector from "./components/ListSelector";
+import App from "./components/App";
+import "./css/style.css";
 
-render(<ListPicker />, document.querySelector("#main"));
+render(<App />, document.querySelector("#main"));
