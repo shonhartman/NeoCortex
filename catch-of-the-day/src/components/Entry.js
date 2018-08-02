2996b1f920773e50f4b18d6d86da8aaf3faad990
Shaun Hartman <shartman@teamsi.com>
2018-08-02 10:34:00 -0500

    Authentication

--- catch-of-the-day/src/components/Entry.js
+++ catch-of-the-day/src/components/Entry.js
@@ -12,10 +12,19 @@ class Entry extends React.Component {
         owner: null
     }
 
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if(user) {
+                this.authHandler({ user });
+            }
+        })
+    }
+
     authHandler = async (authData) => {
-        // 1. Looko up the current list in the firebase database
+
+        // 1. Look up the current list in the firebase database
         const list = await base.fetch(this.props.listId, { context: this })
-        console.log(list);
+
         // 2. Claim it if there is no owner
         if (!list.owner) {
             // save it as our own
@@ -28,21 +37,41 @@ class Entry extends React.Component {
             uid: authData.user.uid,
             owner: list.owner || authData.user.uid
         });
-        console.log(authData);
-    };
+    }
+
     authenticate = (provider) => {
         const authProvider = new firebase.auth[`${provider}AuthProvider`]();
         firebaseApp.auth().signInWithPopup(authProvider).then(this.authHandler);
     }
+
+    logout = async () => {
+        await firebase.auth().signOut();
+        this.setState({ uid: null });
+    }
+
     render() {
+        const logout = <button onClick={this.logout}>Log Out</button>
+
         // 1. Check if they are logged in
         if (!this.state.uid) {
             return <Login authenticate={this.authenticate} />
         }
 
+        // 2. Check if they are not the owner of the store
+        if (this.state.uid !== this.state.owner) {
+            return (
+                    <div>
+                        <p>Sorry you are not the owner</p>
+                        {logout}
+                    </div>
+            );
+        }
+
+        // 3. They must be the owner, render the entry component
         return (
             <div className="inventory">
                 <h2>Entry</h2>
+                {logout}
                 {Object.keys(this.props.habits).map(key =>
                     <EditHabitForm
                         key={key}
